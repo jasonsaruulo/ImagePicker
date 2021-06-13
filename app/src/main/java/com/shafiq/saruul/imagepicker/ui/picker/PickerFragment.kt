7ac1f38db581bd83597259581a8e0898693bdb98
Saruul Shafiq <saruul.s@gmail.com>
2021-06-13 09:20:39 +0200

    Requesting storage permission when picker view is started.

--- app/src/main/java/com/shafiq/saruul/imagepicker/ui/picker/PickerFragment.kt
+++ app/src/main/java/com/shafiq/saruul/imagepicker/ui/picker/PickerFragment.kt
@@ -1,10 +1,14 @@
 package com.shafiq.saruul.imagepicker.ui.picker
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.shafiq.saruul.imagepicker.R
@@ -17,6 +21,11 @@ class PickerFragment @Inject constructor() : Fragment() {
     companion object {
         fun newInstance() = PickerFragment()
     }
+
+    private var requestReadExternalStoragePermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            viewModel.onReadExternalStoragePermissionGranted(it)
+        }
 
     @Inject
     lateinit var viewModelFactory: ImagePickerViewModelFactory
@@ -33,7 +42,38 @@ class PickerFragment @Inject constructor() : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        viewModel.state.observe(viewLifecycleOwner, {
+            when (it) {
+                PickerViewModel.State.Content -> {
+                    // TODO: Show images
+                }
+                PickerViewModel.State.ReadExternalStoragePermissionNeeded -> {
+                    showReadExternalStoragePermissionNeeded()
+                }
+                else -> {
+                    // Do nothing
+                }
+            }
+        })
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
+            // TODO: Handle file access for api >= 30 (https://developer.android.com/training/data-storage/manage-all-files)
+        } else {
+            if (PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                )
+            ) {
+                // TODO: Load images
+            } else {
+                requestReadExternalStoragePermissionLauncher.launch(
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                )
+            }
+        }
         return inflater.inflate(R.layout.picker_fragment, container, false)
+    }
+
+    private fun showReadExternalStoragePermissionNeeded() {
+        view?.findViewById<View>(R.id.picker_permission_needed)?.visibility = View.VISIBLE
     }
 }
--- app/src/main/java/com/shafiq/saruul/imagepicker/ui/picker/PickerViewModel.kt
+++ app/src/main/java/com/shafiq/saruul/imagepicker/ui/picker/PickerViewModel.kt
@@ -1,8 +1,24 @@
 package com.shafiq.saruul.imagepicker.ui.picker
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import javax.inject.Inject
 
 class PickerViewModel @Inject constructor() : ViewModel() {
-    // TODO: Implement the ViewModel
+    enum class State {
+        Content,
+        ReadExternalStoragePermissionNeeded
+    }
+
+    private val _state = MutableLiveData<State>()
+    val state: LiveData<State> = _state
+
+    fun onReadExternalStoragePermissionGranted(granted: Boolean) {
+        if (granted) {
+            _state.value = State.Content
+        } else {
+            _state.value = State.ReadExternalStoragePermissionNeeded
+        }
+    }
 }
