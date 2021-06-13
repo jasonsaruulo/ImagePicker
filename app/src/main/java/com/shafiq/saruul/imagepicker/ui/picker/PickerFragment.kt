b278d8314c3cd7ae188aacb5623d4aad2a6cb7e2
Saruul Shafiq <saruul.s@gmail.com>
2021-06-13 09:36:02 +0200

    Loading image uris in picker view.

--- app/src/main/java/com/shafiq/saruul/imagepicker/ui/picker/PickerFragment.kt
+++ app/src/main/java/com/shafiq/saruul/imagepicker/ui/picker/PickerFragment.kt
@@ -1,9 +1,13 @@
 package com.shafiq.saruul.imagepicker.ui.picker
 
 import android.Manifest
+import android.content.ContentUris
 import android.content.Context
 import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -55,7 +59,47 @@ class PickerFragment @Inject constructor() : Fragment() {
                 }
             }
         })
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
+        viewModel.loadImageUris.observe(viewLifecycleOwner, {
+            context?.let {
+                val imageUris = mutableListOf<Uri>()
+
+                val collection =
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                        MediaStore.Images.Media.getContentUri(
+                            MediaStore.VOLUME_EXTERNAL
+                        )
+                    } else {
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                    }
+
+                val projection =
+                    arrayOf(MediaStore.Images.Media._ID, MediaStore.Images.Media.DATE_ADDED)
+                val sortOrder = "${MediaStore.Images.Media.DATE_ADDED} ASC"
+
+                val query = it.contentResolver?.query(
+                    collection,
+                    projection,
+                    null /* selection */,
+                    null /* selectionArgs */,
+                    sortOrder
+                )
+                query?.use { cursor ->
+                    val idColumn = cursor.getColumnIndexOrThrow(MediaStore.Video.Media._ID)
+
+                    while (cursor.moveToNext()) {
+                        val id = cursor.getLong(idColumn)
+
+                        val contentUri: Uri = ContentUris.withAppendedId(
+                            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                            id
+                        )
+                        imageUris += contentUri
+                    }
+                }
+                viewModel.onImageUrisLoaded(imageUris)
+            }
+        })
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             // TODO: Handle file access for api >= 30 (https://developer.android.com/training/data-storage/manage-all-files)
         } else {
             if (PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission(
@@ -63,7 +107,7 @@ class PickerFragment @Inject constructor() : Fragment() {
                     Manifest.permission.READ_EXTERNAL_STORAGE
                 )
             ) {
-                // TODO: Load images
+                viewModel.onReadExternalStoragePermissionGranted(granted = true)
             } else {
                 requestReadExternalStoragePermissionLauncher.launch(
                     Manifest.permission.READ_EXTERNAL_STORAGE
--- app/src/main/java/com/shafiq/saruul/imagepicker/ui/picker/PickerViewModel.kt
+++ app/src/main/java/com/shafiq/saruul/imagepicker/ui/picker/PickerViewModel.kt
@@ -1,8 +1,10 @@
 package com.shafiq.saruul.imagepicker.ui.picker
 
+import android.net.Uri
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.shafiq.saruul.imagepicker.SingleLiveEvent
 import javax.inject.Inject
 
 class PickerViewModel @Inject constructor() : ViewModel() {
@@ -13,12 +15,18 @@ class PickerViewModel @Inject constructor() : ViewModel() {
 
     private val _state = MutableLiveData<State>()
     val state: LiveData<State> = _state
+    private val _loadImageUris = SingleLiveEvent<Void?>()
+    val loadImageUris: LiveData<Void?> = _loadImageUris
 
     fun onReadExternalStoragePermissionGranted(granted: Boolean) {
         if (granted) {
-            _state.value = State.Content
+            _loadImageUris.call()
         } else {
             _state.value = State.ReadExternalStoragePermissionNeeded
         }
+    }
+
+    fun onImageUrisLoaded(imageUris: List<Uri>) {
+        // TODO: Show images
     }
 }
